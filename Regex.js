dec557f02ce2049b8070b464487de4bf6b0498b2
Jair montenegro <jairmontenegrof@gmail.com>
2022-06-21 22:09:52 -0300

    removed DS_Store

--- Regex.js
+++ Regex.js
@@ -1,7 +1,7 @@
-/**/
-/* expresiones regulares 
+/*ESTA PAGINA TE AYUDA A ENTENDER UNA REGEX Y TAMBIEN TE AYUDA A TESTEARLA https://regexr.com/5b2o7 */  
+/* EXPRESIONES REGUALARES 
 
-Sirven para encontrar buscar o remplezar texto
+Sirven para encontrar, buscar, coincidir o remplezar texto
 
 metodo .test() : 
         nos ayuda a encontrar un patron que le pasamos mediante una cadena al metodo.  Ejemplo 
@@ -34,7 +34,7 @@ si queremos encontrar en nuestra exp una palabra sin importar si esta con mayusc
 sigamos con el ejemplo y le agregaremos mas palabras, 
 */
 
-        let myStringFlag = "Jair is Learning REACT para Conseguir TRABAJO "
+        let myStringFlag = "Jair is Learning REACT para Conseguir TRABAJO " // spanglish my bro 
         let myRegexFLag =  /learning|react|conseguir|trabajo/i
         let resultFlag = myString.test(myRegex)
         /*todas devolveran true*/
@@ -44,7 +44,7 @@ sigamos con el ejemplo y le agregaremos mas palabras,
 se diferencia entre el .test() en que ya no devuelve true sino la plabra que buscamos coincidir ejemplo 
 */
 
-        let myStringMatch = "Jair is Learning REACT para Conseguir TRABAJO "
+        let myStringMatch = "Jair is Learning REACT para Conseguir TRABAJO " // spanglish my bro 
         let myRegexMatch =  /conseguir/i
         let resulMatch = myString.match(myRegex); 
         /*Nos devolvera por consola el  ["conseguir"]*/
@@ -157,7 +157,7 @@ Corregir la expresión regular /<.*>/ para que devuelva la etiqueta HTML <h1> y 
         let yRegex = /<h.*?>/; 
         let resulta = text.match(yRegex);
         console.log(resulta)
-/* efectivamente la consola nos retornara solo <hi> 
+/* efectivamente la consola nos retornara solo <h1> 
 
 /*USANDO EL CARACTER ^ (caret) FUERA DE UN RANGO O CONJUNTO linea 114 */
 /*
@@ -223,10 +223,127 @@ equivalente a  [^A-Za-z0-9_]*/
                 console.log(result9)  //17
 
 /*Tambien podemos coincidir o buscar todos los espacios en blanco usando \s no solo hace coincidir los espacios en blanco si no los
-carriage return [ \r\t\f\n\v]*/ 
+carriage return [ \r\t\f\n\v]
+*/ 
 
                 let sample = "Whitespace is important in separating words";
                 let countWhiteSpace = /\s/g; 
-                let result10 = sample.match(countWhiteSpace);
+                let result10 = sample.match(countWhiteSpace);//nos mostrara tods los espacios 
 
+/*Tambien podemos coincidir o buscar todo menos los espacios en blanco usando \S carriage return tampoco sera coincidido  [^ \r\t\f\n\v] ejemplo 
+
+*/ 
+                let sample2 = "Whitespace is important in separating words";
+                let countNonWhiteSpace = /\S/g; //nos mostrara un array con todos los valores que no sean espacios, tabulaciones o return carriage
+                let result11 = sample2.match(countNonWhiteSpace);
+
+/*Tambien podemos especificar el numero menor y mayor de coincidencias  { , }
+*/
+        /* nota: recordemos que se usa el signo + para buscar uno o mas caracteres 
+         y * para buscar 0 o mas caracteres revisar linea  130 y 138 respectivamente
+        ejemplo 
+         */
+                let ohStr = "Ohhh no";
+                let ohRegex = /oh{3,6}\sno/gi ; // aqui lo estamos haciendo coincidir las h con un rango de 3 a 6 (recuerda separarlos por coma (,) y no por guion (-)
+
+                let result12 = ohRegex.test(ohStr);
+                console.log(result12)  
+
+
+/*Tambien podemos especificar solo el numero menor de coincidencias   { ,};
+                ejemplo 
+                */
+                let haStr = "Hazzzzah";
+                let haRegex = /haz{4,}ah/i;  // coincidira con strings que tengan 4 o mas z en este caso 
+                let result13 = haRegex.test(haStr); // mostrara true 
+
+
+
+/*Tambien podemos especificar el numero exacto de coincidencias  {};
+ejemplo 
+*/
+                let timStr = "Timmmmber";
+                let timRegex = /tim{4}ber/i;  // si tiene mas o menos seria false
+                let result14 = timRegex.test(timStr);
+                console.log(result14) // true 
+
+/*COMPROBAR PATRONES QUE PUEDEN O NO EXISTIR ?
+Se puede hacer mediante la colocacion de un signo de interrigacion (?) comprobando de 0 o 1 los elementos precedentes ante la posible existencia de patron.
+EJEMPLO:  
+*/
+                let favWord = "favorite";
+                let favRegex = /favou?rite/; // nos mostrara true, porque en este caso la u precede al signo de interrogacion lo cual hace que pueda estar o no. 
+                let result15 = favRegex.test(favWord);
+                console.log(result15)
+
+/* LOOKAHEAD POSITIVO Y NEGATIVO
+
+LOOKAHEAD : patrones que le indican a js que busque por anticipado para verificar patrones mas adelante 
+
+
+LOOKAHEAD POSITIVO (?=patron): buscara para asgurarse que el elemento este en el patron de busqueda pero no lo coincidira.
+LOOKAHEAD POSITIVO (?!patron): buscara para asgurarse que el elemento NO este en el patron de busqueda pero no lo coincidira, devolviendo el patron si se 
+devuelve si la parte del lookhaead negativo no esta prestente. 
+
+*/
+                let sampleWord = "abc123";
+                let pwRegex = /(?=\w{6})(?=\D*\d{2})/; // estamos buscando que el paton tenga al menos 5 caracteres y dos numeros con dos lookhaheads positivos.
+                let result16 = pwRegex.test(sampleWord);
+                console.log(result16)
+
+
+/*COMPROBAR AGRUPACIONES MIXTAS*/
+
+
+/*Cuando necesitemos comprobar grupos de caracteres utilizando regex USAMOS (|) con un operator  or 
+ejemplo: 
+        ejercicio : Corrige la expresión regular para que compruebe los nombres de Franklin Roosevelt o Eleanor Roosevelt de una manera sensible a mayúsculas y minúsculas y haciendo concesiones para los segundos nombres.
+
+Luego, corrige el código para que la expresión regular que has creado se compruebe con myString y devuelva true o false dependiendo de si la expresión regular coincide.
+SOLUICION  : 
+*/  
+                let myString3 = "Franklin D. Roosevelt";
+                let myRegex2 = /(franklin d.|eleanor) roosevelt/i; 
+                let result17 = myRegex2.test(myString3); 
+                let result18 = myString3.match(myRegex);
+                console.log(result17) //true con las dos posibilidades 
+                console.log(result18) // "Franklin D. Roosevelt" y "Eleanor Roosevelt" dependiendo de la cadena que este en myString3   
+
+/*GRUPOS DE CAPTURA*/
+
+/*Se usan para capturar el patron de expresion mediante el uso de parentecis. si fuese una palabra con caracteres alfa numericos seria (\w+)
+la subacadena que coincide con el grupo de busqueda se guarda en una variable temporal a la que podemos acceder dentro de la misma regex 
+usando una barra invertida y dentro del GRUPO DE CAPTURA. EJ: (\w+)\1, estos grupos se enumeran de izquierda a derecha emepzando por el 1
+EJEMPLO 
+Utiliza los grupos de captura en reRegex para que coincida con una cadena que conste sólo del mismo número repetido exactamente tres veces separado por espacios.
+*/
+                let repeatNum = "42 42 42";
+                let reRegex = /^(\d+)\s\1\s\1$/; // aqui estamos capturando 1 grupo y lo estamos reutliziando como "variable en las otras comprobaciones"
+                let result19 = reRegex.test(repeatNum);
+
+
+/*GRUPOS DE CAPTURA BUSCAR Y REEMPLAZAR*/
+
+/*Para buscar y remplazar podemos usar .replace() donde recibe dos parametros el primero es la expresion regular y el segundo lo que queremos remplazar*/
+/*dicho de esta manera .replace(regex, "textToModify"), tambien se puede usar grupos de captura en la cadena de remplazo con el signo dolar $:  recuerdar las comllas en lo que quieran modificar
+EJEMPLO :
+Escribe una expresión regular fixRegex utilizando tres grupos de captura que buscarán cada palabra en la cadena one two three. Luego actualiza la variable replaceText para reemplazar one two three con la cadena three two one y asigna el resultado a la variable result.*/
+
+                let str = "one two three";
+                let fixRegex = /(\w+)\s(\w+)\s(\w+)/; 
+                let replaceText ="$3 $2 $1" ; 
+                let result20 = str.replace(fixRegex, replaceText);
+                console.log(result20)    //por la consola mostrara el three two one           
+
+/*ELIMINAR EL ESPACIO EN BLANCO DEL INICIO Y DEL FINAL
+
+EJEMPLO: queremos eliminar los espacios, tanto de inicio como del final. Lo debemos hacer es buscar caputar el primer y ultimo espacio con 
+(^ respectivamente $ ) variable global porque se repite y el operador or(ya que no es consecutivo) ! 
+*/
+                let hello = "   Hello, World!  ";
+                let wsRegex = /^\s+|\s+$/g;     
+                let result21 = hello.replace(wsRegex, "");  
+                 console.log(result21)  //la consola nos mostrara la cadena Hello, world!
+
+/**/
 /**/
