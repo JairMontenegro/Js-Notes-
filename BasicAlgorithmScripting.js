550198cbc1de3c5cf0c541b50b45914c4b53614a
Jair montenegro <jairmontenegrof@gmail.com>
2022-07-01 22:59:25 -0300

    excersise 13

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -305,17 +305,65 @@ let resultA = frankenSplice([1, 2, 3], [4, 5, 6], 1);
 console.log(resultA) // [ 4, 1, 2, 3, 5, 6 ]
 
 
-//SEGUNDO 
+//SEGUNDO OPERADOR SPREAD 
 
 function frankenSplice(arr1, arr2, n) {
   
-  let newArray = arr2.slice()
-  newArray.splice (n, 0, ...arr1)
+  let newArray = arr2.slice() //iniciamos igual que en el ejemplo anterior 
+  newArray.splice (n, 0, ...arr1)  // en vez de iterar copiamos directamente el arreglo con el metodo spread y lo "insertamos" 
+  //en newArrat 
   return newArray 
 } 
-let resulta = frankenSplice([1, 2, 3], [4, 5, 6], 1);
+let resultad = frankenSplice([1, 2, 3], [4, 5, 6], 1); // exactamente el mismo resultado 
 
-console.log(result)
+console.log(resultad)
+
+
+/*                                                      EJERCICIO NUMERO 12                                              */
+
+
+/*
+Quita todos los valores falsos de un arreglo.
+
+Los valores falsos en JavaScript son false, null, 0, "", undefined y NaN.
+
+Sugerencia: Intenta convertir cada valor a booleano.*/
+
+function bouncer(arr) {
+
+  let newArr = []
+  for ( let i = 0 ; i < arr.length; i++){ //iteramos 
+   if (Boolean (arr[i]) ){ // convertimos todos los valores del parametro arr a boleanos 
+    newArr.push(arr[i]) //  si los valores son verdaderos y no falsos los empujamos al neuvo arreglo 
+   }
+
+}
+return newArr // lo retornamos  
+}
+
+console.log(bouncer([false, null, 0, NaN, undefined, ""]));
+// puede ser util https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Boolean
+// recordar valors " falsy " false, null, 0, "", undefined y NaN.
+
+
+
+
+
+
+/*                                                      EJERCICIO NUMERO 13                                             */
+
+
+/*
+Dónde pertenezco
+Devuelve el índice mas bajo en el que un valor (segundo argumento) debe ser insertado en un arreglo (primer argumento) 
+una vez que éste haya sido ordenado. El valor devuelto debe ser un número.
+
+Por ejemplo, getIndexToIns([1,2,3,4], 1.5) debe devolver 1 por que este valor es más grande que 1 (índice 0), 
+pero menor que 2 (índice 1).
+
+De esta forma, getIndexToIns([20,3,5], 19) debe devolver 2 porque una vez ordenado el arreglo, éste se verá así [3,5,20] 
+y 19 es menor que 20 (índice 2) y mayor que 5 (índice 1). */
+
 
 
 /* */
