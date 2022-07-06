2bbd56967cf84653c4c3239cc98013bb58c62405
Jair montenegro <jairmontenegrof@gmail.com>
2022-07-05 21:56:37 -0300

    opp

--- OPP.js
+++ OPP.js
@@ -1,4 +1,4 @@
-// PROGRAMACION ORIENTADA OBJETOS 
+//                                                                  PROGRAMACION ORIENTADA OBJETOS 
 
 
 /* Repasar como crear y como acceder a un objeto mediante notacion de punto (dot notation) */
@@ -19,12 +19,12 @@ dog.sayLegs();
 
 
 
-/* FUNCIONES CONSTRUCTOR */
+/*                                                                   FUNCIONES CONSTRUCTOR                                                          */
 
 /* Construyen nuevos objetos, y definen propiedades y comportamientos que van a tener los nuevos objetos
 se puede pensar como un modelo para la creacion de nuevos objetos
 
-- SE TIENEN QUE ESCRIBIR CON LA PRIMERA LETRA MAYUSUCLA PARA DIFERENCIARLAS DE FUNCIONES QUE NO SEAN CONSTRUCTORS
+- SE TIENEN QUE ESCRIBIR CON LA PRIMERA LETRA MAYUSCULA PARA DIFERENCIARLAS DE FUNCIONES QUE NO SEAN CONSTRUCTORS
 - Utilizan la palabra clave this para establecer propiedades del objeto que crearán. Dentro del constructor, this se 
     refiere al nuevo objeto que creará.
 - Los Constructors definen propiedades y comportamientos en vez de devolverlos como un valor como lo harían otras funciones. 
@@ -63,8 +63,8 @@ function Dog(name, color ) {
     console.log(terrier)// { name: 'Firulais Son', color: 'Gray', numLegs: 4 }
 
 
-/*INSTANCE OF    / instanceof
-
+/*                                                      INSTANCE OF    / instanceof                                             */
+/*
 gracias a este operardor nos permite corroborar si un objeto fue credado a partr de otro devolviendonos TRUE  FALSE 
 EJEMPLO.
 */
@@ -79,13 +79,154 @@ function House(numBedrooms) {
 
 
 
-/*PROPIEDDES DIRECTAS */
+/*                                                          PROPIEDDES DIRECTAS                                                                 */
 
 
-/*  cuando una instancia de objeto */
+/* En nuestro ejemplo de Dog nos damos cuenta que tenemos 3 propiedades name color y numlegs. 
+y son directas porque estan definidas directamente en la isntancia del objeto.
+
+cada objeto creado a partir de ese constructor tendra su porpia copia de estas propiedades.         
+
+ejemplo en esta iteracion a traves del obejto con  for in podemos iterar a travez de las propiedades y 
+asignarlas a un arreglo previamente creado. 
+*/
+
+function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
+  }
+  
+  let canary = new Bird("Tweety");
+  let ownProps = [];
+  
+  for (let property in canary){
+    if(canary.hasOwnProperty(property)){
+      ownProps.push(property)
+    }
+  }
+  
+  console.log(ownProps)
 
 
 
+/*                                           PROPTOTYPE PARA REDUCIR EL CODIGO DUPLICADO                                                           */
+
+
+/* prototype se usa para compartir las propiedades que puedan tener en todas las instancias de un objeto. 
+si tienen el mismo valor */
+
+
+function Dog(name) {
+    this.name = name;
+}
+Dog.prototype.numLegs = 4 ; // se pueden pensar como en una receta para crear objetos. 
+
+let beagle = new Dog("Snoopy");
+let bulterrier  = new Dog("Perralay")
+  console.log(beagle) // { name: 'Snoopy' }
+  console.log(bulterrier.numLegs) // 4
+
+
+
+/*ITERAR SONRE TODAS LAS PROPIEDADES YA SEAN DIRECTAS O PROTOTYPE
+
+con nuestro ejemplo seri de la siguiente manera.
+*/
+
+function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype.numLegs = 4;
+  
+  let beagle2 = new Dog("Snoopy");
+  
+  let ownProps2 = [];
+  let prototypeProps = [];
+  
+  
+  for(let property in beagle){
+    if(Dog.hasOwnProperty(property)){
+      ownProps.push(property)
+    }else{
+      prototypeProps.push(property)
+    }
+  }
+  
+  console.log(ownProps)
+  console.log(prototypeProps)
+
+
+
+  /*                                                PROPIEDAD CONSTRUCTOR (CONSTRUCTOR PROPERTY)                                         */
+
+  /*Esta propiedad hace referencia al constructor que creo que la instancia y se usa para verificar  */
+  
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  function joinDogFraternity(candidate) {
+    if (candidate.constructor===Dog){ // asi seria su uso y su resultado seria si dog hace parte del constructor que pasamos como paramtro 
+    return  true
+   
+    }else{
+      return false
+    }
+  
+  }
+  
+  let bolt = new Dog ("Bolty")
+
+  console.log(joinDogFraternity(bolt)) // true 
+
+
+
+/*                                                       PROTOTYPE A UN OBJETO                                                                 */
+
+/*Una buena practica con prototype es cambiar todas las declaraciones prototype por un objeto
+
+    esto es muy util cuando tenemos que agregar muchas propiedades al prototype, ya que seria muy tedioso agregar una a una. 
+    entonces basciamente las agregamos todas a un objeto.prototype como el siguiente ejemplo. 
+*/
+
+function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype = {
+    numLegs: 2,
+    eat : () => {console.log("i love to eat kibble")},
+    describe : function () {console.log(`Hello mi name is ${this.name}`)}
+  };
+  
+  let santasLittleHelper = new Dog ("Santa's Little Helper")
+  
+  console.log(santasLittleHelper.name) // Santa's Little Helper
+  console.log(santasLittleHelper.eat()) // i love to eat kibble
+  console.log(santasLittleHelper.describe()) // Hello mi name is Santa's Little Helper
+
+/*Un cambio crucial cuando hay se establece el prototype en un nuevo objeto es que la propiedad constructor ya no funciona al ser sobreescrita
+para solucionar esto lo que debemos hacer es volver a definir la propiedad constructor 
+ */
+
+function Dog(name) {
+  this.name = name;
+}
+Dog.prototype = {
+  constructor : Dog, // agregar la propiedad constructor y de esa manera ya se puede comprobar si es instancia de un objeto constructor
+  numLegs: 4,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
+
+
+/* */
+/* */
 /* */
 /* */
 /* */
