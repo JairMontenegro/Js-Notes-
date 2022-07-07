2e4fa752e204c2d1a8d3f0898ce593b98ba50569
Jair montenegro <jairmontenegrof@gmail.com>
2022-07-07 00:05:33 -0300

    start functional programming

---
+++ FunctionalPrograming.js
@@ -0,0 +1,16 @@
+//                                                         PROGRAMACION FUNCIONAL 
+
+
+/* 
+
+La programación funcional es un estilo de programación donde las soluciones son simples, 
+funciones aisladas, sin ningún efecto secundario fuera del ámbito de la función: INPUT -> PROCESS -> OUTPUT
+
+La programación funcional se refiere a:
+
+Funciones aisladas: sin dependencia alguna del estado del programa, el cual incluye variables globales sujetas a cambios
+Funciones puras: una misma entrada siempre da la misma salida
+
+Funciones con efectos secundarios limitados: cualquier cambio o mutación en el estado del programa fuera de la función 
+son cuidadosamente controlados
+*/
