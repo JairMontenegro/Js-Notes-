53c4749284ba47aa0eaffb5a902735f75a5acf23
Jair montenegro <jairmontenegrof@gmail.com>
2022-07-21 18:32:18 -0300

    phone number validator

--- ObtainingTheCertification.js
+++ ObtainingTheCertification.js
@@ -108,7 +108,7 @@ function rot13(str) {
       }else{
         result += letter
       }
-    })
+    })  
   return result 
   }
 
@@ -117,7 +117,58 @@ function rot13(str) {
 
 
 
-  /**/
-/**/
+  /*                                      VALIDADOR DE NUMEROS TELEFONICOS  PROYECTO 4 
+
+  Devuelve true si la cadena pasada concuerda con un número de teléfono válido en Estados Unidos.
+
+El usuario puede completar el campo del formulario de la forma que elija, siempre que tenga el formato de un número estadounidense válido
+Los siguientes ejemplos son de formatos válidos para números estadounidenses (consulte las pruebas a continuación para otras variantes):
+
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+Para este desafío se te presentará una cadena como 800-692-7753 o 8oo-six427676;laskdjf.
+Tu trabajo es validar o rechazar el número de teléfono estadounidense basado en cualquier 
+combinación de los formatos proporcionados arriba. El código de área es obligatorio. Si el código de país es proporcionado,
+debes confirmar que el código de país es 1. Devuelve true si la cadena es un número de teléfono estadounidense valido; de lo contrario devuelve false.
+*/
+function telephoneCheck(str) {
+ 
+  if(str.indexOf("(")=== -1 && str.indexOf(")") > -1){
+    return false
+  }
+  if(str[0]== "-"){
+    return false
+  }
+  
+  if (str.indexOf(")") - str.indexOf("(") >= 5){
+    return false
+  }
+ 
+  if (str.match(/-/g) && str.match(/-/g).length > 2) {
+     return false;
+ }
+ 
+  let  tlf = str.replace(/-| /g,"")
+   if(tlf.indexOf("(") < tlf.indexOf(")") ){
+     tlf = tlf.replace(/\(|\)/g,"")
+     }
+ 
+ 
+  if (tlf.length === 10 ){
+    return true 
+  }else if( tlf.length === 11 && tlf[0] == 1 ){
+    return true 
+  }
+   return false
+ }
+ 
+ console.log(telephoneCheck("55 55-55-555-5"));
+
+
+
 /**/
 /**/
